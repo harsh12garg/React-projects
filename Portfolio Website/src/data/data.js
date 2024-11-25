b4762879d4872c628415d2ada9939c5f5dfce414
HARSH GARG <133204589+harsh12garg@users.noreply.github.com>
2024-11-25 20:11:51 +0530

    Add files via upload

---
+++ Portfolio Website/src/data/data.js
@@ -0,0 +1,132 @@
+import WorkIm from "../assets/projects/workImg.jpeg";
+import RealEst from "../assets/projects/realestate.jpg";
+import Remcare from "../assets/projects/remcare.png";
+import HEat from "../assets/projects/input-onlinepngtools.png";
+import Airbnb from "../assets/projects/airbnbori.jpg";
+import Dashboard from "../assets/projects/dashboard.png";
+import Remover from "../assets/projects/remover.jpg";
+import Graph from "../assets/projects/graph__2.jpeg";
+import Atm from "../assets/projects/atm.png";
+import Candy from "../assets/projects/candy.png";
+import Dodge from "../assets/projects/Dodge.png";
+import pipe from "../assets/projects/pipes.png";
+import DB from "../assets/projects/db.png";
+import veteran from "../assets/projects/veteran.jpg";
+import Scrapper from "../assets/projects/scraper.png";
+import Twitter from "../assets/projects/Twitter.jpg";
+export const data = [
+  {
+    id: 1,
+    name: "Remote Care (Android + Web + Iot)",
+    image: Remcare,
+    github: "not",
+    live: "#",
+    link2: "",
+  },
+  {
+    id: 2,
+    name: "H.Eats (React)",
+    image: HEat,
+    github: "#",
+    live: "#",
+    link2: "#",
+  },
+  {
+    id: 4,
+    name: "Twitter Clone Next.js+Tailwind",
+    image: Twitter,
+    github: "#",
+    live: "#",
+    link2: "",
+  },
+  {
+    id: 5,
+    name: "Airbnb Clone (Mern)",
+    image: Airbnb,
+    github: "#",
+    live: "#",
+    link2: "",
+  },
+
+  {
+    id: 6,
+    name: "React-DashBoard",
+    image: Dashboard,
+    github: "#",
+    live: "#",
+    link2: "",
+  },
+  {
+    id: 7,
+    name: "React Background Remover",
+    image: Remover,
+    github: "#",
+    live: "#",
+    link2: "",
+  },
+  {
+    id: 8,
+    name: "Network Analysis (C++)",
+    image: Graph,
+    github: "#",
+    live: "#",
+    link2: "",
+  },
+  {
+    id: 9,
+    name: "ATM Machine (OOP)",
+    image: Atm,
+    github: "#",
+    live: "",
+    link2: "",
+  },
+  {
+    id: 10,
+    name: "Candy Crush (OOP)",
+    image: Candy,
+    github: "#",
+    live: "",
+    link2: "",
+  },
+  {
+    id: 11,
+    name: "Dodge EM (C++)",
+    image: Dodge,
+    github: "#",
+    live: "",
+    link2: "",
+  },
+  {
+    id: 12,
+    name: "Restaurant Management (C++ OS)",
+    image: pipe,
+    github: "#",
+    live: "",
+    link2: "",
+  },
+  {
+    id: 13,
+    name: "NGO School DB System",
+    image: DB,
+    github: "#",
+    live: "",
+    link2: "",
+  },
+
+  {
+    id: 14,
+    name: "Veteran Meet (MERN)",
+    image: veteran,
+    github: "#",
+    live: "",
+    link2: "",
+  },
+  {
+    id: 15,
+    name: "Daraz Scrapper (Python and Mysql)",
+    image: Scrapper,
+    github: "#",
+    live: "",
+    link2: "",
+  },
+];
